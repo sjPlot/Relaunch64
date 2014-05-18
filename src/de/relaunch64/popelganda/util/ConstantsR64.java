3ee56df5d333d92387c4552ed3e0054328c0e0c0
Daniel Lüdecke <d.luedecke@uke.de>
2014-05-18 16:07:05 +0200

    build version number

--- src/de/relaunch64/popelganda/util/ConstantsR64.java
+++ src/de/relaunch64/popelganda/util/ConstantsR64.java
@@ -46,7 +46,7 @@ public class ConstantsR64 {
     /**
      * This variable stores the current programme and build version number
      */
-    public static final String BUILD_VERSION = "3.0.1 (Build 20140519)";
+    public static final String BUILD_VERSION = "3.0.2 (Build 20140519)";
     /**
      * This variable stores the application title that appears in the menu bar or log
      */
@@ -55,7 +55,7 @@ public class ConstantsR64 {
      * This variable stores the application title that appears in the main window
      * Add "Release Candidate" or "Beta" to this title
      */
-    public static final String APPLICATION_TITLE = "Relaunch64";
+    public static final String APPLICATION_TITLE = "Relaunch64 (devel-build)";
     /**
      * This constants stores the website-address where the app can be downloaded
      */
