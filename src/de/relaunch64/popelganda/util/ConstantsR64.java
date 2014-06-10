77bb77410fcbce6b93544fb1ae9784811299cffa
Daniel Lüdecke <d.luedecke@uke.de>
2014-06-10 08:50:56 +0200

    build-version for revel-snapshot

--- src/de/relaunch64/popelganda/util/ConstantsR64.java
+++ src/de/relaunch64/popelganda/util/ConstantsR64.java
@@ -46,7 +46,7 @@ public class ConstantsR64 {
     /**
      * This variable stores the current programme and build version number
      */
-    public static final String BUILD_VERSION = "3.1.1 (Build 20140609)";
+    public static final String BUILD_VERSION = "3.1.1 (Build 20140610)";
     /**
      * This variable stores the application title that appears in the menu bar or log
      */
@@ -55,7 +55,7 @@ public class ConstantsR64 {
      * This variable stores the application title that appears in the main window
      * Add "Release Candidate" or "Beta" to this title
      */
-    public static final String APPLICATION_TITLE = "Relaunch64";
+    public static final String APPLICATION_TITLE = "Relaunch64 (developer-snapshot)";
     /**
      * This constants stores the website-address where the app can be downloaded
      */
